f3ac6c4c04c24bd7db736723786e69ef827d5a33
jamaluddin <hasinkhan410@gmail.com>
2022-11-07 20:34:43 +0500

    chatapp

---
+++ chatapp/src/App.jsx
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import { Link,Routes,Route,Router} from 'react-router-dom'
+
+import Home from './components/home'
+import Login from './components/login'
+import Signup from './components/signup'
+import NotFound from './components/notfound'
+
+
+function App() {
+  return (
+    <div >
+  
+  <nav>
+<ul>
+<li>  <Link  to="/"> Home</Link></li>
+<li> <Link to="/Login"> Login </Link></li>
+<li>  <Link to="/Signup"> Signup</Link></li>
+</ul>
+  </nav>
+
+  <Routes>
+<Route
+path="/Home"
+element={<Home />}
+errorElement={<NotFound />}
+/>
+<Route
+path="/Login"
+element={<Login />}
+errorElement={<NotFound />}
+/>
+
+
+<Route
+  path="/Signup"
+  element={<Signup />}
+  errorElement={<NotFound />}
+  />
+  </Routes>
+    </div>
+  );
+}
+
+export default App;
